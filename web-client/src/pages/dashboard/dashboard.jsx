fc2124bc03c14a1c8de837f116f39fb582035964
filipi <filipi87@gmail.com>
2021-10-12 22:11:39 -0300

    Adding button to back to home

--- web-client/src/pages/dashboard/dashboard.jsx
+++ web-client/src/pages/dashboard/dashboard.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect } from 'react';
-import { Typography, Container } from '@material-ui/core'
+import { Typography, Container, Button } from '@material-ui/core'
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
 
+import { HOME } from '../../router/router-constants';
+import { useHistory } from 'react-router-dom';
 import { useCallContext } from '../../contexts/calls-context';
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
-        height: '100vh',
+        height: '90vh',
         width: '100vw'
+    },
+    homeButton: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        margin: '10px'
     }
 }));
 
 const Dashboard = () => {
   const classes = useStyles();
+  const history = useHistory();
   const { state, dispatchGetRooms, dispatchGetRoom } = useCallContext();
 
   useEffect(() => {
     dispatchGetRooms()
   }, []);
+
+  const goToHomePage = () => {
+      history.push(HOME);
+  };
 
   const columns = [
       {
@@ -27,14 +40,14 @@ const Dashboard = () => {
           headerName: 'Room Id',
           sortable: false,
           filterable: false,
-          flex: 1,
+          width: 200,
       },
       {
           field: 'url',
           headerName: 'Room URL',
           sortable: false,
           filterable: false,
-          width: 200,
+          flex: 1,
       },
   ];
 
@@ -42,18 +55,23 @@ const Dashboard = () => {
     return null
   }
   return (
-    <Container className={classes.root}>
-      <DataGrid
-          rows={state?.rooms}
-          columns={columns}
-          getRowId={(row) => row.roomId}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-          autoPageSize
-          disableSelectionOnClick
-          disableColumnFilter
-      />
-    </Container>
+    <>
+      <Button type="button" color="primary" variant="contained" className={classes.homeButton} onClick={() => { goToHomePage() }}>
+          Home
+      </Button>
+      <Container className={classes.root}>
+        <DataGrid
+            rows={state?.rooms}
+            columns={columns}
+            getRowId={(row) => row.roomId}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            autoPageSize
+            disableSelectionOnClick
+            disableColumnFilter
+        />
+      </Container>
+    </>
   );
 };
 
